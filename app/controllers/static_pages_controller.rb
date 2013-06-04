5e26cc4c309f26ec4bc4aae4846824612eacc762
Ells Campbell <ells.cubed@gmail.com>
2013-06-04 15:20:11 -0400

    d3 integration with rails, bootstrap, and angularJS

---
+++ app/controllers/static_pages_controller.rb
@@ -0,0 +1,15 @@
+class StaticPagesController < ApplicationController
+
+  def home
+  end
+
+  def help
+  end
+
+  def about
+  end
+
+  def contact
+  end
+
+end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+
+  def full_title(page_title)
+    base_title = "Vax | Gamifying Vaccination"
+    if page_title.empty?
+      base_title
+    else
+      "#{base_title} | #{page_title}"
+    end
+  end
 end
