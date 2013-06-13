01894dd1a79eb553d57ad723d76d3c95d3bf7b49
Ells Campbell <ells.cubed@gmail.com>
2013-06-13 14:02:25 -0400

    heroku debugging

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,12 +10,10 @@
 // WARNING: THE FIRST BLANK LINE MARKS THE END OF WHAT'S TO BE PROCESSED, ANY BLANK LINE SHOULD
 // GO AFTER THE REQUIRES BELOW.
 //
-//= require bootstrap
-//= require bootstrap-dropdown
 //= require angular
-//= require angular-resource
 //= require jquery
 //= require jquery-ui
 //= require jquery_ujs
+//= require bootstrap
 //= require_tree .
 
