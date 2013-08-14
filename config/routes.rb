88248432a9079b4dd99a546454accfcdf2d002ab
Ells Campbell <ells.cubed@gmail.com>
2013-08-14 14:55:51 -0400

    remove "intervention" + routing help to static tutorial page

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ VaxGame::Application.routes.draw do
   resources :scores
 
   match '/home', to: 'static_pages#home'
-  match '/help', to: 'static_pages#help'
+  match '/help', to: 'static_pages#tutorial'
   match '/about', to: 'static_pages#about'
   match '/contact', to: 'static_pages#contact'
 
@@ -15,7 +15,6 @@ VaxGame::Application.routes.draw do
   match '/signout', to: 'sessions#destroy', via: :delete
 
   match '/test', to: 'static_pages#test'
-  match '/intervention', to: 'static_pages#intervention'
   match '/tutorial', to: 'static_pages#tutorial'
 
   match '/game',       to: 'scores#game'
