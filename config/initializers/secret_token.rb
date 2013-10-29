7646efb5c07551a345abf402316fb650885cf707
Ellsworth Campbell <ells@psu.edu>
2013-10-28 22:36:28 -0400

    delete for security

--- config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-VaxGame::Application.config.secret_token = '961efbbf1996b557fec9dcfcd8c53cb9961c9f47e99538bb6aef612c316635112cab3fc3bcd78138626831b436966d4deda0b6c4bac7898078a2a7fbad6a6a2d'
