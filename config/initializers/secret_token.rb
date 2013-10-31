863f2642b26be96ad607f865fc59e29cca090836
Ells Campbell <ells.cubed@gmail.com>
2013-10-31 16:13:17 -0400

    secret token to environment variables
    http://daniel.fone.net.nz/blog/2013/05/20/a-better-way-to-manage-the-rai
    ls-secret-token/

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,12 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-VaxGame::Application.config.secret_token = '961efbbf1996b557fec9dcfcd8c53cb9961c9f47e99538bb6aef612c316635112cab3fc3bcd78138626831b436966d4deda0b6c4bac7898078a2a7fbad6a6a2d'
+
+# local secret token are generated each time, security not of primary concern
+# deployment secret token is set manually with heroku config:set SECRET_TOKEN=[not_the_actual_secret_token]
+
+VaxGame::Application.config.secret_token = if Rails.env.development? or Rails.env.test?
+  ('x' * 30)
+else
+ ENV['SECRET_TOKEN']
+end
