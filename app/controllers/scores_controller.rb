c50888a64d84d9ad69dc6590b7ed69ad0813f156
Ells Campbell <ells.cubed@gmail.com>
2014-02-05 16:05:14 -0500

    process scores held in database for comparisons

--- app/controllers/scores_controller.rb
+++ app/controllers/scores_controller.rb
@@ -1,14 +1,27 @@
 class ScoresController < ApplicationController
+  def index
+    @original = Score.find_by_scenario("Random Networks")
+    @work = Score.find_by_scenario("Workplace / School")
+    @theater = Score.find_by_scenario("Movie Theater / Lecture Hall")
+    @shop = Score.find_by_scenario("Endless Queue")
+    @restaurant = Score.find_by_scenario("Restaurant")
+    @club = Score.find_by_scenario("Organization")
+  end
+
   def new
     @score = Score.new
+
+    @original = Score.find_by_scenario("Random Networks")
+    @work = Score.find_by_scenario("Workplace / School")
+    @theater = Score.find_by_scenario("Movie Theater / Lecture Hall")
+    @shop = Score.find_by_scenario("Endless Queue")
+    @restaurant = Score.find_by_scenario("Restaurant")
+    @club = Score.find_by_scenario("Organization")
   end
 
   def create
     @score = Score.new(params[:score])
     @score.save
-    #if @score.save
-    #  redirect_to :action => :new
-    #end
   end
 
 end
