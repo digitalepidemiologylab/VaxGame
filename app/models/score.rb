d557ccd4d93db42d5d3e8aec82f0089096764cac
Diego Muracciole <diegomuracciole@gmail.com>
2021-02-27 14:26:38 -0300

    Build static project

--- app/models/score.rb
+++
@@ -1,5 +0,0 @@
-class Score < ActiveRecord::Base
-  attr_accessible :difficulty, :infected, :quarantined, :realtime, :saved, :scenario
-
-
-end
--- test/integration/authentication_pages_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class AuthenticationPagesTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
-end
